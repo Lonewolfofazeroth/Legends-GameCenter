064191ca830bf47ce69128c000bb09fe20c96ed5
LoneWolfofAzeroth <87613462+Lonewolfofazeroth@users.noreply.github.com>
2023-01-08 14:18:00 +0330

    Update entry.client.tsx

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,5 @@
 import { hydrate } from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
-
+import { aaa } from "user-db";
+import { gamelst } from "sm/gamelist"
 hydrate(<RemixBrowser />, document);
